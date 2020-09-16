ddce195afac3de72f5950bbdd588fef39d687ffc
Justin Edmund <justin@jedmund.com>
2020-09-16 03:28:14 -0700

    Add weapons routes
    These are for GridWeapons though

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
             resources :party, only: [:index, :create, :show, :destroy]
             resources :users, only: [:create, :show]
             resources :search, only: [:index]
+
+            post 'weapons', to: 'grid_weapons#create'
+            delete 'weapons', to: 'grid_weapons#destroy'
         end
     end
 end
