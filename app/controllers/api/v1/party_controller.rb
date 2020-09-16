aa7820dd221e5969ac7521efd706cefaadf53d38
Justin Edmund <justin@jedmund.com>
2020-09-16 03:42:12 -0700

    Add PartiesController

---
+++ app/controllers/api/v1/party_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::PartyController < ActionController::API
+    before_action :set, except: ['create']
+
+    def index
+        parties = Party.all
+    end
+
+    def create
+        @party = Party.new(shortcode: random_string)
+        render :show, status: :created if @party.save!
+    end
+
+    def show
+        ap @party
+        ap @party
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def random_string
+        numChars = 6
+        o = [('a'..'z'), ('A'..'Z'), (0..9)].map(&:to_a).flatten
+        return (0...numChars).map { o[rand(o.length)] }.join
+    end
+
+    def set
+        @party = Party.where(shortcode: params[:id])
+    end
+end
