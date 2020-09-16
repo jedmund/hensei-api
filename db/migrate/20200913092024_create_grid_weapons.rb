1fdd0017b3272270ebbfc8765bc522cda00099c5
Justin Edmund <justin@jedmund.com>
2020-09-16 03:26:22 -0700

    Update grid weapon table
    Add `mainhand`
    Fix `belongs_to` by changing to `references`

--- db/migrate/20200913092024_create_grid_weapons.rb
+++ db/migrate/20200913092024_create_grid_weapons.rb
@@ -1,13 +1,14 @@
 class CreateGridWeapons < ActiveRecord::Migration[6.0]
     def change
-            t.belongs_to :composition, type: :uuid
-            
         create_table :grid_weapons, id: :uuid, default: -> { "gen_random_uuid()" } do |t|
+            t.references :party, type: :uuid
             t.references :weapon, type: :uuid
+            
             t.references :weapon_key1, class_name: 'WeaponKey', type: :uuid
             t.references :weapon_key2, class_name: 'WeaponKey', type: :uuid
 
             t.integer :uncap_level
+            t.boolean :mainhand
             t.integer :position
 
             t.timestamps
