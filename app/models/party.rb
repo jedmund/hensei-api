865534338277cf46ccbf25749abcad5f9f9c83d5
Justin Edmund <justin@jedmund.com>
2020-09-16 23:48:10 -0700

    Fix association

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,5 +1,5 @@
 class Party < ApplicationRecord
 ##### ActiveRecord Associations
     belongs_to :user, optional: true
-    has_many :weapons, through: :grid_weapons
+    has_many :weapons, foreign_key: "party_id", class_name: "GridWeapon"
 end
