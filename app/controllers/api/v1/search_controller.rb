64073bdae8a2ff5e025fd721c893602ff5390c51
Justin Edmund <justin@jedmund.com>
2020-09-18 07:16:04 -0700

    Limit weapon search to 10 at once

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SearchController < ApplicationController
     def index
         logger.debug params
         if params[:query].present?
-            @weapons = Weapon.search(params[:query])
+            @weapons = Weapon.search(params[:query]).limit(10)
         else
             @weapons = Weapon.all
         end
