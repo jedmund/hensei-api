c78af4758245197e84383f785f78c332af984d38
Justin Edmund <justin@jedmund.com>
2020-09-25 10:51:08 -0700

    Add validation error serializers

---
+++ app/helpers/validation_error_serializer.rb
@@ -0,0 +1,33 @@
+class ValidationErrorSerializer
+    def initialize(record, field, details)
+        @record = record
+        @field = field
+        @details = details
+    end
+
+    def serialize
+        {
+            resource: resource,
+            field: field,
+            code: code
+        }
+    end
+
+    private
+
+    def resource
+        @record.class.to_s
+    end
+
+    def field
+        @field.to_s
+    end
+
+    def code
+        @details[:error].to_s
+    end
+
+    def underscored_resource_name
+        @record.class.to_s.gsub('::', '').underscore
+    end
+end
