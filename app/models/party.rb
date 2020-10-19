b44b8e9b701509c72b6f6d7903730e2b391bb522
Justin Edmund <justin@jedmund.com>
2020-10-18 22:25:17 -0700

    Update Party model to have summons

--- app/models/party.rb
+++ app/models/party.rb
@@ -2,4 +2,5 @@ class Party < ApplicationRecord
 ##### ActiveRecord Associations
     belongs_to :user, optional: true
     has_many :weapons, foreign_key: "party_id", class_name: "GridWeapon"
+    has_many :summons, foreign_key: "party_id", class_name: "GridSummon"
 end
--- app/views/api/v1/parties/base.json.rabl
+++ app/views/api/v1/parties/base.json.rabl
@@ -2,6 +2,10 @@ object :party
 
 attributes :id, :user_id, :shortcode
 
-node :grid do |p|
+node :weapons do |p|
     partial('grid_weapons/base', :object => p.weapons)
+end
+
+node :summons do |p|
+    partial('grid_summons/base', :object => p.summons)
 end
