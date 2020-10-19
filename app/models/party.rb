201a5f4157ec8966d2dbd62cd8462145413020ac
Justin Edmund <justin@jedmund.com>
2020-10-19 05:10:32 -0700

    Add characters to Party model and template

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,6 +1,7 @@
 class Party < ApplicationRecord
 ##### ActiveRecord Associations
     belongs_to :user, optional: true
+    has_many :characters, foreign_key: "party_id", class_name: "GridCharacter"
     has_many :weapons, foreign_key: "party_id", class_name: "GridWeapon"
     has_many :summons, foreign_key: "party_id", class_name: "GridSummon"
 end
--- app/views/api/v1/parties/base.json.rabl
+++ app/views/api/v1/parties/base.json.rabl
@@ -2,6 +2,10 @@ object :party
 
 attributes :id, :user_id, :shortcode
 
+node :characters do |p|
+    partial('grid_characters/base', :object => p.characters)
+end
+
 node :weapons do |p|
     partial('grid_weapons/base', :object => p.weapons)
 end
