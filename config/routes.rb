a63027aff691f19393666bca56fd9130b95c7f90
Justin Edmund <justin@jedmund.com>
2020-10-18 22:25:35 -0700

    Update routes for search and summons

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,19 @@ Rails.application.routes.draw do
     namespace :api, defaults: { format: :json } do
         namespace :v1 do
             resources :parties, only: [:index, :create, :show, :destroy]
-            resources :search, only: [:index]
             resources :users, only: [:create, :show]
 
             post 'check/email', to: 'users#check_email'
             post 'check/username', to: 'users#check_username'
 
+            get 'search/weapons', to: 'search#weapons'
+            get 'search/summons', to: 'search#summons'
+
             post 'weapons', to: 'grid_weapons#create'
             delete 'weapons', to: 'grid_weapons#destroy'
+
+            post 'summons', to: 'grid_summons#create'
+            delete 'summons', to: 'grid_summons#destroy'
         end
     end
 end
