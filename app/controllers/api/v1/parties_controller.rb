374e294a337537f9979bf1f70337b6c74948ea9e
Justin Edmund <justin@jedmund.com>
2022-02-23 19:22:42 -0800

    Change permitted param for extra toggle

--- app/controllers/api/v1/parties_controller.rb
+++ app/controllers/api/v1/parties_controller.rb
@@ -71,6 +71,6 @@ class Api::V1::PartiesController < Api::V1::ApiController
     end
 
     def party_params
-        params.require(:party).permit(:user_id, :is_extra, :name, :description, :raid_id)
+        params.require(:party).permit(:user_id, :extra, :name, :description, :raid_id)
     end
 end
