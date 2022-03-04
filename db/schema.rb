c78a28c6761a8e370efb90f52d2f516d0a66aef4
Justin Edmund <justin@jedmund.com>
2022-03-04 01:17:04 -0800

    Add new fields to user object

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_092208) do
+ActiveRecord::Schema.define(version: 2022_03_04_091218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -186,6 +186,9 @@ ActiveRecord::Schema.define(version: 2022_03_03_092208) do
     t.integer "granblue_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture"
+    t.string "language"
+    t.boolean "private"
   end
 
   create_table "weapon_keys", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
