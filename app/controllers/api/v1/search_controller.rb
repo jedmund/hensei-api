88f316d6a7872b684526b80465ef41c03c72220c
Justin Edmund <justin@jedmund.com>
2022-03-08 17:43:40 -0800

    Implement JP search and trigram for EN

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,28 +1,42 @@
 class Api::V1::SearchController < Api::V1::ApiController
     def characters
+        locale = params[:locale] || 'en'
+
         if params[:query].present?
-            excludes = params[:excludes] ? 
-                params[:excludes].split(',').map { |e| "%#{e.gsub(/\([^()]*\)/, '').strip}%" } : ''
-            
-            @characters = Character.where("name_en ILIKE ? AND name_en NOT ILIKE ALL(ARRAY[?])", "%#{params[:query]}%", excludes).limit(10)
-            # @characters = Character.search(query).limit(10)
+            if locale == 'ja'
+                @characters = Character.jp_search(params[:query]).limit(10)
+            else
+                # @characters = Character.where("name_en ILIKE ? AND name_en NOT ILIKE ALL(ARRAY[?])", "%#{params[:query]}%", excludes).limit(10)
+                @characters = Character.en_search(params[:query]).limit(10)
+            end
         else
             @characters = Character.all
         end
     end
 
     def weapons
+        locale = params[:locale] || 'en'
+
         if params[:query].present?
-            @weapons = Weapon.search(params[:query]).limit(10)
+            if locale == 'ja'
+                @weapons = Weapon.jp_search(params[:query]).limit(10)
+            else
+                @weapons = Weapon.en_search(params[:query]).limit(10)
+            end
         else
             @weapons = Weapon.all
         end
     end
 
     def summons
+        locale = params[:locale] || 'en'
+
         if params[:query].present?
-            excludes = params[:excludes] ? params[:excludes].split(',').each { |e| "!#{e}" }.join(' ') : ''
-            @summons = Summon.search(params[:query]).limit(10)
+            if locale == 'ja'
+                @summons = Summon.jp_search(params[:query]).limit(10)
+            else
+                @summons = Summon.en_search(params[:query]).limit(10)
+            end
         else
             @summons = Summon.all
         end
