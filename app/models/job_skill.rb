d1eba7aed881302e3a9b26c9a824c7e97988638a
Justin Edmund <justin@jedmund.com>
2022-12-02 11:20:22 -0800

    Add == and update search

--- app/models/job_skill.rb
+++ app/models/job_skill.rb
@@ -3,25 +3,29 @@ class JobSkill < ApplicationRecord
 
   belongs_to :job
 
-  pg_search_scope :en_search, 
-        against: :name_en,
-        using: {
-            tsearch: {
-                prefix: true,
-                dictionary: "simple"
-            }
-        }
+  pg_search_scope :en_search,
+                  against: :name_en,
+                  using: {
+                    tsearch: {
+                      prefix: true,
+                      dictionary: "simple",
+                    },
+                  }
 
-    pg_search_scope :jp_search, 
-        against: :name_jp,
-        using: {
-            tsearch: {
-                prefix: true,
-                dictionary: "simple"
-            }
-        }
+  pg_search_scope :jp_search,
+                  against: :name_jp,
+                  using: {
+                    tsearch: {
+                      prefix: true,
+                      dictionary: "simple",
+                    },
+                  }
 
   def display_resource(skill)
     skill.name_en
+  end
+
+  def ==(other)
+    self.class == other.class && @id == other.id
   end
 end
