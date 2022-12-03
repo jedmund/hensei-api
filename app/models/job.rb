bd33ee44fc062f0cc4753c53af510532b2d579e1
Justin Edmund <justin@jedmund.com>
2022-12-03 11:44:09 -0800

    Update Job model to reference base_job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,12 @@
 class Job < ApplicationRecord
   belongs_to :party
-    
+
+  belongs_to :base_job,
+             foreign_key: 'base_job_id',
+             class_name: 'Job',
+             optional: true
+
   def display_resource(job)
-      job.name_en
+    job.name_en
   end
 end
