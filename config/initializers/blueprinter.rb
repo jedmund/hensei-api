c102d68dd7b050d669ffefd8ebc53e53da915090
Justin Edmund <justin@jedmund.com>
2022-12-21 03:31:24 -0800

    Update blueprinter.rb

--- config/initializers/blueprinter.rb
+++ config/initializers/blueprinter.rb
@@ -4,5 +4,5 @@ require 'oj' # you can skip this if OJ has already been required.
 
 Blueprinter.configure do |config|
   config.generator = Oj # default is JSON
-  # config.sort_fields_by = :definition
+  config.sort_fields_by = :definition
 end
