7720d5400f24302014749e5104bab1eaae3df434
Justin Edmund <justin@jedmund.com>
2022-12-21 01:17:04 -0800

    Configure Blueprinter

---
+++ config/initializers/blueprinter.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'oj' # you can skip this if OJ has already been required.
+
+Blueprinter.configure do |config|
+  config.generator = Oj # default is JSON
+  # config.sort_fields_by = :definition
+end
