8f9bd0077e727b02682354e7b22d925add0b7d92
Justin Edmund <justin@jedmund.com>
2023-01-07 07:51:54 -0800

    Add new fields to output of GridCharacter

--- app/blueprints/api/v1/grid_character_blueprint.rb
+++ app/blueprints/api/v1/grid_character_blueprint.rb
@@ -11,11 +11,38 @@ module Api
       view :nested do
         fields :position, :uncap_level, :perpetuity
 
-        field :awakening do |c|
+        field :transcendence_step, if: ->(_fn, obj, _opt) {
+          obj.character.ulb
+        } do |c|
+          c.transcendence_step
+        end
+
+        field :awakening, if: ->(_fn, obj, _opt) {
+          !obj[:awakening].nil?
+        } do |c|
           {
-            type: c.awakening_type,
-            level: c.awakening_level
+            type: c.awakening[:type],
+            level: c.awakening[:level]
           }
+        end
+
+        field :over_mastery, if: ->(_fn, obj, _opt) {
+          !obj.ring1['modifier'].nil? && !obj.ring2['modifier'].nil?
+        } do |c|
+          rings = []
+
+          rings.push(c.ring1) if !c.ring1['modifier'].nil?
+          rings.push(c.ring2) if !c.ring2['modifier'].nil?
+          rings.push(c.ring3) if !c.ring3['modifier'].nil?
+          rings.push(c.ring4) if !c.ring4['modifier'].nil?
+
+          rings
+        end
+
+        field :aetherial_mastery, if: ->(_fn, obj, _opt) {
+          !obj.earring['modifier'].nil?
+        } do |c|
+          c.earring
         end
 
         association :character, name: :object, blueprint: CharacterBlueprint
