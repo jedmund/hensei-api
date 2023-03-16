b31d9f8b9e7980325f77ca7e3881e0c222c89ac4
Justin Edmund <justin@jedmund.com>
2023-03-16 05:12:03 -0700

    Remove files that don't belong here

--- db/migrate/20230315113015_add_nicknames_to_summons.rb
+++
@@ -1,6 +0,0 @@
-class AddNicknamesToSummons < ActiveRecord::Migration[7.0]
-  def change
-    add_column :summons, :nicknames_en, :string, array: true, default: [], null: false
-    add_column :summons, :nicknames_jp, :string, array: true, default: [], null: false
-  end
-end
