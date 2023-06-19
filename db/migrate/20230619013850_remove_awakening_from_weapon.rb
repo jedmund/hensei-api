d399d8f1beeaceee3dcc29ab22ad2c1100c7dca0
Justin Edmund <justin@jedmund.com>
2023-06-19 02:52:39 -0700

    Fix migration

--- db/migrate/20230619013850_remove_awakening_from_weapon.rb
+++ db/migrate/20230619013850_remove_awakening_from_weapon.rb
@@ -1,6 +1,5 @@
 class RemoveAwakeningFromWeapon < ActiveRecord::Migration[7.0]
   def change
     remove_column :weapons, :awakening, :boolean
-    remove_column :weapons, :awakening_types, :integer, array: true
   end
 end
