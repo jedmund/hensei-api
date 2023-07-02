4ded81c7042166bdad3c5ea8f98671962923bcf9
Justin Edmund <justin@jedmund.com>
2023-07-02 00:42:46 -0700

    Remove old code
    This has been sitting around for a long time and we haven't uncommented it yet, so we can delete it

--- app/controllers/api/v1/parties_controller.rb
+++ app/controllers/api/v1/parties_controller.rb
@@ -23,19 +23,6 @@ module Api
         party.user = current_user if current_user
         party.attributes = party_params if party_params
 
-        # unless party_params.empty?
-        #   party.attributes = party_params
-        #
-        #   # TODO: Extract this into a different method
-        #   job = Job.find(party_params['job_id']) if party_params['job_id'].present?
-        #   if job
-        #     job_skills = JobSkill.where(job: job.id, main: true)
-        #     job_skills.each_with_index do |skill, index|
-        #       party["skill#{index}_id"] = skill.id
-        #     end
-        #   end
-        # end
-
         if party.save!
           return render json: PartyBlueprint.render(party, view: :created, root: :party),
                         status: :created
