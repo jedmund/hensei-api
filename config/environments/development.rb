aee9e48a69551dc77b97be0dddfb0f1fcac7f125
Justin Edmund <justin@jedmund.com>
2025-01-08 12:12:56 -0800

    Syntax updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,8 @@
-require "active_support/core_ext/integer/time"
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "staging-api.granblue.team"
+  config.hosts << 'staging-api.granblue.team'
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
@@ -20,10 +20,10 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
+  if Rails.root.join('tmp/caching-dev.txt').exist?
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -57,4 +57,23 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  #
+
+  logger = ActiveSupport::Logger.new(STDOUT)
+  # To support a formatter, you must manually assign a formatter from the config.log_formatter value to the logger.
+  logger.formatter = config.log_formatter
+  # config.logger is the logger that will be used for Rails.logger and any
+  # related Rails logging such as ActiveRecord::Base.logger.
+  # It defaults to an instance of ActiveSupport::TaggedLogging that wraps an
+  # instance of ActiveSupport::Logger which outputs a log to the log/ directory.
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
+  # config.log_level defines the verbosity of the Rails logger.
+  # This option defaults to :debug for all environments.
+  # The available log levels are: :debug, :info, :warn, :error, :fatal, and :unknown
+  # config.log_level = :debug
+  # config.log_tags accepts a list of: methods that the request object responds to,
+  # a Proc that accepts the request object, or something that responds to to_s.
+  # This makes it easy to tag log lines with debug information like subdomain and request id -
+  # both very helpful in debugging multi-user production applications.
+  config.log_tags = [:request_id]
 end
