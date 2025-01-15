eb44c04eb5fff803afb532e9e9d574fccdc53f56
Justin Edmund <justin@jedmund.com>
2025-01-15 02:02:42 -0800

    Add recruits column to weapons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_10_070255) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_15_094528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "pg_trgm"
@@ -469,6 +469,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_10_070255) do
     t.string "nicknames_jp", default: [], null: false, array: true
     t.boolean "transcendence", default: false
     t.date "transcendence_date"
+    t.string "recruits"
     t.index ["name_en"], name: "index_weapons_on_name_en", opclass: :gin_trgm_ops, using: :gin
     t.index ["recruits_id"], name: "index_weapons_on_recruits_id"
   end
