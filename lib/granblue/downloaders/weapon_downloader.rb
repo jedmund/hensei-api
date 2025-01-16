29c52469a3373a173b1d0b82d44d3eedff2e6c4e
Justin Edmund <justin@jedmund.com>
2025-01-15 19:12:02 -0800

    Update logic for downloaders
    These are responsible for downloading variants (01, 02, etc)

--- lib/granblue/downloaders/weapon_downloader.rb
+++ lib/granblue/downloaders/weapon_downloader.rb
@@ -3,7 +3,46 @@
 module Granblue
   module Downloader
     class WeaponDownloader < BaseDownloader
+      def download
+        weapon = Weapon.find_by(granblue_id: @id)
+        return unless weapon
+
+        download_variants(weapon)
+      end
+
       private
+
+      def download_variants(weapon)
+        # All weapons have base variant
+        variants = [@id]
+
+        # Add transcendence variants if available
+        if weapon.transcendence
+          variants.push("#{@id}_02", "#{@id}_03")
+        end
+
+        log_info "Downloading weapon variants: #{variants.join(', ')}" if @verbose
+
+        variants.each do |variant_id|
+          download_variant(variant_id)
+        end
+      end
+
+      def download_variant(variant_id)
+        log_info "-> #{variant_id}" if @verbose
+        return if @test_mode
+
+        SIZES.each_with_index do |size, index|
+          path = download_path(size)
+          url = build_variant_url(variant_id, size)
+          process_download(url, size, path, last: index == SIZES.size - 1)
+        end
+      end
+
+      def build_variant_url(variant_id, size)
+        directory = directory_for_size(size)
+        "#{@base_url}/#{directory}/#{variant_id}.jpg"
+      end
 
       def object_type
         'weapon'
