2979ffb1e52c3cd26c299bb82fb12b691627f775
Justin Edmund <justin@jedmund.com>
2025-02-07 01:34:55 -0800

    Configure Sidekiq
    Create job for cleaning up party previews

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,12 @@
-# Fetch environment variables with defaults if not set
-redis_url = ENV.fetch('REDIS_URL', 'redis://localhost')
-redis_port = ENV.fetch('REDISPORT', '6379')
-
-# Combine URL and port (adjust the path/DB as needed)
-full_redis_url = "#{redis_url}/0"
+redis_url = ENV.fetch('REDIS_URL', 'redis://localhost:6379/0')
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: full_redis_url }
+  config.redis = { url: redis_url }
+  config.death_handlers << ->(job, ex) do
+    Rails.logger.error("Preview generation job #{job['jid']} failed with: #{ex.message}")
+  end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: full_redis_url }
+  config.redis = { url: redis_url }
 end
