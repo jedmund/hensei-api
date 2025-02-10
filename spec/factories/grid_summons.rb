cdfdec0fa79123d7bd7a738a9aa5b57a89d83253
Justin Edmund <justin@jedmund.com>
2025-02-10 05:30:04 -0800

    Create GridSummon factory

---
+++ spec/factories/grid_summons.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+# spec/factories/grid_summons.rb
+
+FactoryBot.define do
+  factory :grid_summon do
+    association :party
+    # Use the canonical (seeded) Summon record.
+    # Make sure your CSV canonical data (loaded via canonical.rb) includes a Summon with the specified granblue_id.
+    summon { Summon.find_by!(granblue_id: '2040433000') }
+    position { 1 }
+    uncap_level { 3 }
+    transcendence_step { 0 }
+    main { false }
+    friend { false }
+    quick_summon { false }
+  end
+end
