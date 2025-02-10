3e0bab98e91b3030921264dc7641f2e8498d84c7
Justin Edmund <justin@jedmund.com>
2025-02-10 07:04:49 -0800

    Add GridWeapons and Parties factories

--- spec/factories/parties.rb
+++ spec/factories/parties.rb
@@ -1,5 +1,21 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :party do
-    
+    association :user
+
+    # Use a sequence for unique party names (optional).
+    sequence(:name) { |n| "Party #{n}" }
+    description { Faker::Lorem.sentence }
+    extra { false }
+    full_auto { false }
+    auto_guard { false }
+    charge_attack { true }
+    clear_time { 0 }
+    button_count { 0 }
+    chain_count { 0 }
+    turn_count { 0 }
+    visibility { 1 }
+    # Note: Shortcode and edit_key will be auto-generated via before_create callbacks.
   end
 end
