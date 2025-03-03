28a6b1894e68380602737efc3abafe01380d4549
Justin Edmund <justin@jedmund.com>
2025-03-02 16:29:05 -0800

    Delete db/migrate/20250301143956_add_wiki_raw_to_characters.rb (#194)

--- db/migrate/20250301143956_add_wiki_raw_to_characters.rb
+++
@@ -1,7 +0,0 @@
-class AddWikiRawToCharacters < ActiveRecord::Migration[8.0]
-  def change
-    add_column :characters, :wiki_raw, :text
-    add_column :characters, :game_raw_en, :text
-    add_column :characters, :game_raw_jp, :text
-  end
-end
