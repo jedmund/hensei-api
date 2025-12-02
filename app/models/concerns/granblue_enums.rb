24d8d20ff85794dccfbf437ec567f5199531f72b
Justin Edmund <justin@jedmund.com>
2025-12-02 04:07:38 -0800

    add CHARACTER_SEASONS and CHARACTER_SERIES enums

--- app/models/concerns/granblue_enums.rb
+++ app/models/concerns/granblue_enums.rb
@@ -31,4 +31,33 @@ module GranblueEnums
     Primal: 5,
     None: 6
   }.freeze
+
+  # Character seasons - represents gacha availability window (when you can pull)
+  CHARACTER_SEASONS = {
+    Standard: 1,
+    Valentine: 2,
+    Formal: 3,
+    Summer: 4,
+    Halloween: 5,
+    Holiday: 6
+  }.freeze
+
+  # Character series - represents character identity/pool membership (can have multiple)
+  CHARACTER_SERIES = {
+    Standard: 1,
+    Grand: 2,
+    Zodiac: 3,
+    Promo: 4,
+    Collab: 5,
+    Eternal: 6,
+    Evoker: 7,
+    Saint: 8,
+    Fantasy: 9,
+    Summer: 10,
+    Yukata: 11,
+    Valentine: 12,
+    Halloween: 13,
+    Formal: 14,
+    Event: 15
+  }.freeze
 end
