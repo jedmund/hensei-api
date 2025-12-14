1caffecdadd4525162de43bcd44f2ed76a2fff3b
Justin Edmund <justin@jedmund.com>
2025-12-14 01:48:15 -0800

    add next-api.granblue.team to allowed hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,7 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << 'staging-api.granblue.team'
+  config.hosts << 'next-api.granblue.team'
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
