95af3cec96d0e7c44eeb2c8c92b0e82ad1532ce8
Justin Edmund <justin@jedmund.com>
2026-03-05 05:44:31 -0800

    harden weak model specs

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -7,4 +7,19 @@ RSpec.describe Job, type: :model do
     it { is_expected.to have_many(:skills).class_name('JobSkill') }
     it { is_expected.to belong_to(:base_job).class_name('Job').optional }
   end
+
+  describe 'display_resource' do
+    it 'returns name_en' do
+      job = create(:job, name_en: 'Dark Fencer')
+      expect(job.display_resource(job)).to eq('Dark Fencer')
+    end
+  end
+
+  describe 'search' do
+    it 'can search by English name' do
+      job = create(:job, name_en: 'Berserker')
+      results = Job.en_search('Berserker')
+      expect(results).to include(job)
+    end
+  end
 end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -4,13 +4,23 @@ require 'rails_helper'
 
 RSpec.describe Skill, type: :model do
   describe 'enums' do
-    it 'defines skill_type enum' do
+    it 'defines skill_type enum with correct values' do
       expect(described_class.skill_types).to eq(
         'character' => 0,
         'charge_attack' => 1,
         'summon' => 2,
         'weapon' => 3
       )
+    end
+
+    it 'supports querying by skill_type' do
+      weapon_skill = create(:skill, :weapon, name_en: 'Weapon Skill')
+      character_skill = create(:skill, :character, name_en: 'Character Skill')
+
+      expect(Skill.weapon).to include(weapon_skill)
+      expect(Skill.weapon).not_to include(character_skill)
+      expect(Skill.character).to include(character_skill)
+      expect(Skill.character).not_to include(weapon_skill)
     end
   end
 
@@ -31,6 +41,7 @@ RSpec.describe Skill, type: :model do
     it 'is invalid without name_en' do
       skill = build(:skill, name_en: nil)
       expect(skill).not_to be_valid
+      expect(skill.errors[:name_en]).to include("can't be blank")
     end
   end
 end
