873d586418931ab11fa0b5aa0539aa9e9dc7419a
Justin Edmund <justin@jedmund.com>
2026-03-12 14:17:12 -0700

    fix element mapping in raid factory traits

--- spec/factories/raids.rb
+++ spec/factories/raids.rb
@@ -6,31 +6,32 @@ FactoryBot.define do
     sequence(:name_en) { |n| "Test Raid #{n}" }
     name_jp { "テストレイド" }
     sequence(:slug) { |n| "test-raid-#{n}" }
-    element { 1 } # Fire
+    element { 2 } # Fire (Wind=1, Fire=2, Water=3, Earth=4, Dark=5, Light=6)
+    player_count { 18 }
     level { 150 }
 
     trait :fire do
-      element { 1 }
-    end
-
-    trait :water do
       element { 2 }
     end
 
-    trait :earth do
+    trait :water do
       element { 3 }
     end
 
-    trait :wind do
+    trait :earth do
       element { 4 }
     end
 
+    trait :wind do
+      element { 1 }
+    end
+
     trait :light do
-      element { 5 }
+      element { 6 }
     end
 
     trait :dark do
-      element { 6 }
+      element { 5 }
     end
 
     trait :null_element do
