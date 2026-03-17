c2ab2a4e4694f795ad7c430625296f6f00f8f2ab
Justin Edmund <justin@jedmund.com>
2026-03-17 02:08:57 -0700

    add variant CRUD controller and blueprint

---
+++ app/blueprints/api/v1/weapon_series_variant_blueprint.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class WeaponSeriesVariantBlueprint < ApiBlueprint
+      fields :has_weapon_keys, :has_awakening, :num_weapon_keys,
+             :augment_type, :element_changeable, :extra
+    end
+  end
+end
