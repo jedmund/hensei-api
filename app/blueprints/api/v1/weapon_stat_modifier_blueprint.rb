6084240b937b5a995f2ba11dad36fdb7d8116cb6
Justin Edmund <justin@jedmund.com>
2026-03-18 07:19:21 -0700

    expose base_min/base_max in weapon stat modifier blueprint

--- app/blueprints/api/v1/weapon_stat_modifier_blueprint.rb
+++ app/blueprints/api/v1/weapon_stat_modifier_blueprint.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class WeaponStatModifierBlueprint < Blueprinter::Base
       identifier :id
-      fields :slug, :name_en, :name_jp, :category, :stat, :polarity, :suffix
+      fields :slug, :name_en, :name_jp, :category, :stat, :polarity, :suffix, :base_min, :base_max
     end
   end
 end
