8c5ed934a9205006837172218478ef6a37829415
Justin Edmund <justin@jedmund.com>
2026-03-19 01:35:26 -0700

    use :list view for all party list endpoints
    drops ~90% of unused fields from explore, user profile,
    favorites, and playlist responses

--- app/blueprints/api/v1/playlist_blueprint.rb
+++ app/blueprints/api/v1/playlist_blueprint.rb
@@ -34,20 +34,15 @@ module Api
       end
 
       view :with_parties do
-        field :parties do |playlist, options|
+        field :parties do |playlist, _options|
           ordered = playlist.parties.includes(
-            :user, :job, { raid: :group },
-            { characters: { character: :character_series_records } },
-            { weapons: { weapon: [:awakenings, :weapon_series, :weapon_series_variant] } },
+            :job, { raid: :group },
+            { characters: :character },
+            { weapons: :weapon },
             { summons: :summon }
           ).order(updated_at: :desc)
 
-          PartyBlueprint.render_as_hash(
-            ordered,
-            view: :preview,
-            current_user: options[:current_user],
-            favorite_party_ids: options[:favorite_party_ids] || Set.new
-          )
+          PartyBlueprint.render_as_hash(ordered, view: :list)
         end
       end
     end
