455c06f139969acd39867ad529cbdf03c4c24fc3
Justin Edmund <justin@jedmund.com>
2026-03-21 09:37:45 -0700

    disable preview monitor scheduled jobs

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,15 +4,16 @@ require 'rufus-scheduler'
 unless defined?(Rails::Console) || Rails.env.test? || File.split($0).last == 'rake'
   scheduler = Rufus::Scheduler.new
 
-  scheduler.every '5m' do
-    PreviewService::GenerationMonitor.check_stalled_jobs
-  end
-
-  scheduler.every '1h' do
-    PreviewService::GenerationMonitor.retry_failed
-  end
-
-  scheduler.every '1d' do
-    PreviewService::GenerationMonitor.cleanup_old_previews
-  end
+  # Preview generation monitor jobs are disabled while the feature is in progress.
+  # scheduler.every '5m' do
+  #   PreviewService::GenerationMonitor.check_stalled_jobs
+  # end
+  #
+  # scheduler.every '1h' do
+  #   PreviewService::GenerationMonitor.retry_failed
+  # end
+  #
+  # scheduler.every '1d' do
+  #   PreviewService::GenerationMonitor.cleanup_old_previews
+  # end
 end
