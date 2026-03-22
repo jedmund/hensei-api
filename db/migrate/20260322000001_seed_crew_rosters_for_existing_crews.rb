4185bec0543d307c0b41a1740c9a95aeda3825a0
Justin Edmund <justin@jedmund.com>
2026-03-22 14:50:49 -0700

    inline SQL in seed migration, avoid model dependency

--- db/migrate/20260322000001_seed_crew_rosters_for_existing_crews.rb
+++ db/migrate/20260322000001_seed_crew_rosters_for_existing_crews.rb
@@ -1,14 +1,36 @@
 class SeedCrewRostersForExistingCrews < ActiveRecord::Migration[8.0]
-  def up
-    Crew.find_each do |crew|
-      captain = crew.crew_memberships.find_by(role: :captain)&.user
-      next unless captain
+  ELEMENTS = {
+    1 => 'Wind',
+    2 => 'Fire',
+    3 => 'Water',
+    4 => 'Earth',
+    5 => 'Dark',
+    6 => 'Light'
+  }.freeze
 
-      CrewRoster.seed_for_crew!(crew, captain)
+  def up
+    crews = execute("SELECT id FROM crews")
+    crews.each do |row|
+      crew_id = row['id']
+
+      captain_id = execute(<<~SQL).first&.dig('user_id')
+        SELECT user_id FROM crew_memberships
+        WHERE crew_id = '#{crew_id}' AND role = 2 AND retired = false
+        LIMIT 1
+      SQL
+      next unless captain_id
+
+      ELEMENTS.each do |element, name|
+        execute(<<~SQL)
+          INSERT INTO crew_rosters (id, crew_id, created_by_id, name, element, items, created_at, updated_at)
+          VALUES (gen_random_uuid(), '#{crew_id}', '#{captain_id}', '#{name}', #{element}, '[]', NOW(), NOW())
+          ON CONFLICT (crew_id, element) DO NOTHING
+        SQL
+      end
     end
   end
 
   def down
-    CrewRoster.delete_all
+    execute("DELETE FROM crew_rosters")
   end
 end
