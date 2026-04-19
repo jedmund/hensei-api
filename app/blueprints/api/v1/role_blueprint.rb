a0b172f271e6648b6e5fe60ba2dcb4a203c8a937
Justin Edmund <justin@jedmund.com>
2026-04-19 00:59:08 -0700

    expose role id in blueprint so dropdown can bind selection

--- app/blueprints/api/v1/role_blueprint.rb
+++ app/blueprints/api/v1/role_blueprint.rb
@@ -3,7 +3,7 @@
 module Api
   module V1
     class RoleBlueprint < ApiBlueprint
-      fields :name_en, :name_jp, :slot_type, :sort_order
+      fields :id, :name_en, :name_jp, :slot_type, :sort_order
     end
   end
 end
