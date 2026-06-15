5da07c022af5e9a2a47346bcb61df7ca4a7da2c9
Justin Edmund <justin@jedmund.com>
2026-06-15 23:59:58 +0900

    Serialize game_icon in character skill version blueprint

--- app/blueprints/api/v1/character_skill_version_blueprint.rb
+++ app/blueprints/api/v1/character_skill_version_blueprint.rb
@@ -6,7 +6,7 @@ module Api
       field(:name) { |version| { en: version.name_en, ja: version.name_jp } }
       field(:description) { |version| { en: version.description_en, ja: version.description_jp } }
 
-      fields :icon, :type_color, :cooldown, :initial_cooldown, :duration_value, :duration_unit,
+      fields :icon, :game_icon, :type_color, :cooldown, :initial_cooldown, :duration_value, :duration_unit,
              :variant_role, :ordinal, :unlock_level, :enhance_levels, :min_uncap,
              :transcendence_stage, :trigger_type, :trigger_value,
              :cant_recast, :one_time_use, :auto_activate, :mimicable, :targets_all
